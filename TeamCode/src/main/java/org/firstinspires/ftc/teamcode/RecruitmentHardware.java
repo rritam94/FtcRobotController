c36f35571dde39e458bd98fd867d536ae86215b6
JakeSteinebronn <jakesteinebronn@gmail.com>
2021-08-03 10:07:03 -0400

    Auto,Tele,Robot Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RecruitmentHardware.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class RecruitmentHardware {
+
+
+    /* Public OpMode members. */
+    public DcMotor leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+//    public Servo    rightClaw   = null;
+
+    public static final double MID_SERVO       =  0.5 ;
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public RecruitmentHardware(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftDrive  = hwMap.get(DcMotor.class, "leftMotor");
+        rightDrive = hwMap.get(DcMotor.class, "rightMotor");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+//        // Define and initialize ALL installed servos.
+//        leftClaw  = hwMap.get(Servo.class, "left_hand");
+//        rightClaw = hwMap.get(Servo.class, "right_hand");
+//        leftClaw.setPosition(MID_SERVO);
+//        rightClaw.setPosition(MID_SERVO);
+    }
+}
+
